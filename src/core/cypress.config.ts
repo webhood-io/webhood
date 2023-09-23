4035033b522d7bda53cee21d8b97bd31f1047efd
markusleh <28872014+markusleh@users.noreply.github.com>
2023-09-23 13:25:33 +0300

    ci(core): add cypress

--- src/core/cypress.config.ts
+++ src/core/cypress.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "cypress"
 
 export default defineConfig({
+  projectId: "robbtp",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
