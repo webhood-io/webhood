a13ae358ee1217479d6547d97f4f63af67a14539
markusleh <28872014+markusleh@users.noreply.github.com>
2023-10-02 21:38:04 +0300

    tests: fix e2e script

--- scripts/setup-e2e.sh
+++ scripts/setup-e2e.sh
@@ -5,9 +5,9 @@ yarn run dev &
 cd ../backend
 cp -r migrations src/pb_migrations
 cd src
-$BACKEND_CMD[@] migrate
-$BACKEND_CMD[@] create_user -u admin -p password123 -e test@example.com
-SCANNER_TOKEN=$($BACKEND_CMD[@] create_scanner_token 2>&1|grep SCANNER_TOKEN|cut -d '=' -f2) 
+go run main.go config.go migrate
+go run main.go config.go create_user -u admin -p password123 -e test@example.com
+SCANNER_TOKEN=$(go run main.go config.go create_scanner_token 2>&1|grep SCANNER_TOKEN|cut -d '=' -f2) 
 # echo as {"SCANNER_TOKEN":"<token>"}
 echo "{\"SCANNER_TOKEN\":\"${SCANNER_TOKEN}\"}" > ../../core/cypress.env.json
 # replace the token in the ".env" file with the one printed by the previous command
