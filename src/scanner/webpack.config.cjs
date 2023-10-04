9736eb567e37ca905f2e94ff0df3ecad617d8ac4
markusleh <28872014+markusleh@users.noreply.github.com>
2023-10-04 21:14:47 +0300

    fix(scanner): add correct entrypoint for new script

--- src/scanner/webpack.config.cjs
+++ src/scanner/webpack.config.cjs
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'production',
-  entry: './src/server.js',
+  entry: './src/main.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
