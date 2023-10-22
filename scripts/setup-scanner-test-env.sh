5324597628816e5439bf624a5e23c381c4ca1087
markusleh <28872014+markusleh@users.noreply.github.com>
2023-10-22 09:32:32 +0300

    ci(scanner): add daemon to backend test script

--- scripts/setup-scanner-test-env.sh
+++ scripts/setup-scanner-test-env.sh
@@ -4,4 +4,4 @@ cd src
 go run main.go config.go migrate
 go run main.go config.go create_scanner_token 2>&1|grep SCANNER_TOKEN >> ../../scanner/.env 
 echo "ENDPOINT=http://127.0.0.1:8090" >> ../../scanner/.env
-go run main.go config.go serve 
+go run main.go config.go serve &
