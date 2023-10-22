ba4a2cebc00a6dfdbccaaf21015fa171277b29dc
markusleh <28872014+markusleh@users.noreply.github.com>
2023-10-22 09:24:03 +0300

    refactor: change API_KEY to SCANNER_TOKEN

--- src/scanner/src/memoryAuthStore.ts
+++ src/scanner/src/memoryAuthStore.ts
@@ -4,10 +4,10 @@ import PocketBase, { BaseAuthStore } from 'pocketbase';
 export class EnvAuthStore extends BaseAuthStore {
     save(token: any, model: any) {
         super.save(token, model);
-        process.env.API_KEY = token;
+        process.env.SCANNER_TOKEN = token;
     }
     get token() {
-        return process.env.API_KEY || ""
+        return process.env.SCANNER_TOKEN || ""
     }
     get isValid() {
         return true
