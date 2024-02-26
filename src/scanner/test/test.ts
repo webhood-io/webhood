4a87d3110a1af031bb0e013728576c709a249712
markusleh <28872014+markusleh@users.noreply.github.com>
2024-02-26 23:02:51 +0200

    tests(scanner): add testing for filtering

--- src/scanner/test/test.ts
+++ src/scanner/test/test.ts
@@ -1,63 +1,86 @@
-import { browserinit, screenshot } from "../src/server"
-import { assert, expect } from 'chai';
-import "mocha" // required for types
+import { browserinit, screenshot } from "../src/server";
+import { assert, expect } from "chai";
+import "mocha"; // required for types
 import { pb } from "../src/server";
 import { WebhoodScannerPageError } from "../src/errors";
+import { filterScans } from "../src/main";
+import { ScansRecord } from "../src/types/pocketbase-types";
 
-
-function randomIntFromInterval(min:number, max:number) { // min and max included 
-  return Math.floor(Math.random() * (max - min + 1) + min)
+function randomIntFromInterval(min: number, max: number) {
+  // min and max included
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 describe("Basic scanner tests", () => {
-      it("should launch browser", async () => {
-        const browser = await browserinit()
-        const connected = browser.connected
-        console.log("Browser is connected: ", connected)
-        expect(browser.connected).to.equal(true); 
-        await browser.close()
-   }).timeout(10000);
-   it("should navigate to google.com", async () => {
-    const browser = await browserinit()
+  it("should launch browser", async () => {
+    const browser = await browserinit();
+    const connected = browser.connected;
+    console.log("Browser is connected: ", connected);
+    expect(browser.connected).to.equal(true);
+    await browser.close();
+  }).timeout(10000);
+  it("should navigate to google.com", async () => {
+    const browser = await browserinit();
     const page = await browser.newPage();
-    await page.goto("https://google.com")
-    expect(browser.connected).to.equal(true); 
+    await page.goto("https://google.com");
+    expect(browser.connected).to.equal(true);
     const finalUrl = await page.evaluate(() => document.location.href);
-    expect(finalUrl).to.equal("https://www.google.com/"); 
-    await browser.close()
-   }).timeout(10000)
-   it("should run screenshot",async () => {
-    const scans = pb.collection("scans")
+    expect(finalUrl).to.equal("https://www.google.com/");
+    await browser.close();
+  }).timeout(10000);
+
+  it("should filter local site", async () => {
+    //todo
+    process.env.SCANNER_NO_PRIVATE_IPS = "true";
+    const scansModel = pb.collection("scans");
+    const localScan = await scansModel.create({
+      url: "https://localhost",
+      status: "pending",
+      slug: `test-${randomIntFromInterval(1, 10000).toString()}`,
+    });
+    const publicScan = await scansModel.create({
+      url: "https://google.com",
+      status: "pending",
+      slug: `test-${randomIntFromInterval(1, 10000).toString()}`,
+    });
+    const scans = [localScan, publicScan] as ScansRecord[];
+    const filtered = await filterScans(scans);
+    expect(scans).to.have.length(2);
+    expect(filtered).to.have.length(1);
+    expect(filtered[0].url).to.equal("https://google.com");
+  });
+  it("should run screenshot", async () => {
+    const scans = pb.collection("scans");
     const data = await scans.create({
-      "url": "https://google.com",
-      "status": "pending",
-      "slug": `test-${randomIntFromInterval(1,10000).toString()}`
-    })
-    const browser = await browserinit()
-    await screenshot(null, data.url, data.id, browser)
-    const updatedData = await scans.getOne(data.id)
-    expect(updatedData.final_url).to.equal("https://www.google.com/")
-    expect(updatedData.status).to.equal("done")
-    expect(updatedData.screenshots).to.have.length(1)
-    expect(updatedData.html).to.have.length(2) // with trace
-    expect(updatedData.error).to.be.empty
-    browser.close()
-   }).timeout(10000)
-   it("should error on unavailable site",async () => {
-    const scans = pb.collection("scans")
+      url: "https://google.com",
+      status: "pending",
+      slug: `test-${randomIntFromInterval(1, 10000).toString()}`,
+    });
+    const browser = await browserinit();
+    await screenshot(null, data.url, data.id, browser);
+    const updatedData = await scans.getOne(data.id);
+    expect(updatedData.final_url).to.equal("https://www.google.com/");
+    expect(updatedData.status).to.equal("done");
+    expect(updatedData.screenshots).to.have.length(1);
+    expect(updatedData.html).to.have.length(2); // with trace
+    expect(updatedData.error).to.be.empty;
+    browser.close();
+  }).timeout(10000);
+  it("should error on unavailable site", async () => {
+    const scans = pb.collection("scans");
     const data = await scans.create({
-      "url": "https://googl.se",
-      "status": "pending",
-      "slug": `test-${randomIntFromInterval(1,10000).toString()}`
-    })
-    const browser = await browserinit()
+      url: "https://googl.se",
+      status: "pending",
+      slug: `test-${randomIntFromInterval(1, 10000).toString()}`,
+    });
+    const browser = await browserinit();
     let error;
     try {
-      await screenshot(null, data.url, data.id, browser)
-    } catch(err) {
-      error=err
+      await screenshot(null, data.url, data.id, browser);
+    } catch (err) {
+      error = err;
     }
-    expect(error).to.an.instanceOf(WebhoodScannerPageError)
-    browser.close()
-   }).timeout(20000)
+    expect(error).to.an.instanceOf(WebhoodScannerPageError);
+    browser.close();
+  }).timeout(20000);
 });
