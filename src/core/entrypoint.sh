070ea566b60d1f283667d2ab26b4e2cf70850c2f
markusleh <28872014+markusleh@users.noreply.github.com>
2024-03-06 23:37:25 +0200

    fix: set api URL default to /

--- src/core/entrypoint.sh
+++ src/core/entrypoint.sh
@@ -19,6 +19,9 @@ echo "SELF_REGISTER: \t $SELF_REGISTER"
 # - API_URL_VALUE
 # - SELF_REGISTER_VALUE
 #
+# if API_URL is not set, we will use the default value "/"
+if [ -z "$API_URL" ]; then
+  API_URL="/"
 find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#http://API_URL_VALUE#$API_URL#g"
 find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#SELF_REGISTER_VALUE#$SELF_REGISTER#g"
 
