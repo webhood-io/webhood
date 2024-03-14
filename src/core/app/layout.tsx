974a14137969e70e02c4193a7abfce6a7a0187de
markusleh <28872014+markusleh@users.noreply.github.com>
2024-03-14 22:07:05 +0200

    fix: same classes for app pages body as in pages directory

--- src/core/app/layout.tsx
+++ src/core/app/layout.tsx
@@ -7,7 +7,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Styled />
-      <body>
+      <body className="min-h-screen bg-white font-sans text-slate-900 antialiased dark:bg-slate-900 dark:text-slate-50">
         <ClientLayout>{children}</ClientLayout>
       </body>
     </html>
