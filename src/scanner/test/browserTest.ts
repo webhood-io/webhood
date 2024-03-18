542d776be0a98eb74b6b4c6fa84ae6723d26e9e9
markusleh <28872014+markusleh@users.noreply.github.com>
2024-03-18 13:06:42 +0200

    ci: no need to await

--- src/scanner/test/browserTest.ts
+++ src/scanner/test/browserTest.ts
@@ -53,7 +53,7 @@ describe("Basic scanner tests", () => {
     } catch (err) {
       error = err;
     }
-    await browser.close();
+    browser.close();
     expect(error).to.an.instanceOf(WebhoodScannerPageError);
   }).timeout(40000);
 });
