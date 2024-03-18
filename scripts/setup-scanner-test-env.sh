6c40a2d75ed3bca96b100a4ba007bd709ed23c16
markusleh <28872014+markusleh@users.noreply.github.com>
2024-03-18 12:22:28 +0200

    ci: add debug logging to scanner

--- scripts/setup-scanner-test-env.sh
+++ scripts/setup-scanner-test-env.sh
@@ -3,5 +3,6 @@ cp -r migrations src/pb_migrations
 cd src
 go run main.go migrate
 go run main.go create_scanner_token 2>&1|grep SCANNER_TOKEN >> ../../scanner/.env 
+echo "LOG_LEVEL=debug" >> ../../scanner/.env
 echo "ENDPOINT=http://127.0.0.1:8090" >> ../../scanner/.env
 go run main.go serve &
